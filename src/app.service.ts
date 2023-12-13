785b3210fbed8c313712e5d6a456f1f90c958914
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-13 16:38:10 -0300

    Refactor getHello method to use async/await

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,24 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<any> {
     const helloMessage = 'Full Cycle Rocks!';
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    console.log(helloMessage);
-    const messageHello = 'Full Cycle Rocks!';
     return 'Full Cycle Rocks!';
   }
 }
