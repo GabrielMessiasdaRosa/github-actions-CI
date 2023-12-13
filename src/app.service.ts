db14b326d54516d411e6c27d52fd1e4b235addf2
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-13 15:51:11 -0300

    Remove unnecessary code in getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello WARUDO! TOKI WO TOMARE! Trying to make a change!';
+    return 'Hello WARUDO! TOKI WO TOMARE!';
   }
 }
