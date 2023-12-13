c3624cc382e3c5d0ecde506c7a559740177b300a
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-13 16:42:43 -0300

    adding any

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   async getHello(): Promise<any> {
-    const helloMessage = 'Full Cycle Rocks!';
-    return 'Full Cycle Rocks!' as any;
+    const helloMessage: any = 'Full Cycle Rocks!' as any;
+    return helloMessage;
   }
 }
