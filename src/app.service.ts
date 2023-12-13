7b2c572759d2d2b028a8d21c21bf957598d92d23
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-13 17:35:19 -0300

    Refactor getHello method in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<any> {
+  getHello() {
     const helloMessage: any = 'Full Cycle Rocks!' as any;
-    return helloMessage;
+    return 'Full Cycle Rocks!';
   }
 }
