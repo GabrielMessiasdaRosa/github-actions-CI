649723fbbd845c554bb97c8a753b0991514ebf40
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-13 16:31:17 -0300

    fix: :bug: Refactor getHello method in AppService
    Refactor getHello method in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,23 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const helloMessage = 'Full Cycle Rocks!';
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    console.log(helloMessage);
+    const messageHello = 'Full Cycle Rocks!';
+    return 'Full Cycle Rocks!';
   }
 }
